c4b842c6ebb65d393d90bda40f84509438191f3f
= <luanfagundes@gmail.com>
2019-10-07 01:21:00 -0300

    a

--- triangulo.js
+++ triangulo.js
@@ -1,3 +1,56 @@
+let oEsq = {
+        posA: 110,
+        tamA: 30,
+        posB: 130,
+        tamB: 220,
+        posC: 240,
+        tamC: 120 
+    },
+    oDir = {
+        posA: 490,
+        tamA: 30,
+        posB: 470,
+        tamB: 220,
+        posC: 370,
+        tamC: 120
+    },
+    rostoGato = {
+        largura: 300,
+        altura: 240,
+        tamanho: 170
+    },
+    oGatoDireito = {
+        largura: 220,
+        altura: 230,
+        tamanho: 30
+    },
+    oGatoEsquerdo = {
+        largura: 380,
+        altura: 230,
+        tamanho: 30
+    },
+    dRetina = {
+        lados: 217,
+        altura: 217,
+        largura: 05,
+        tamanho: 25
+    },
+    eRetina = {
+        lados: 380,
+        altura: 217,
+        largura: 05,
+        tamanho: 25
+    },
+    nariz = {
+        posA: 300,
+        tamA: 300,
+        posB: 270,
+        tamB: 275,
+        posC: 330,
+        tamC: 275 
+    };
+
+
 verificaTipoTriangulo = (verticeAX, verticeBX, verticeCX, verticeAY, verticeBY, verticeCY) => { 
     var distanciaAB = Math.abs(Math.pow((verticeAX - verticeBX),2)) + Math.abs(Math.pow((verticeAY - verticeBY),2));
     var distanciaAC = Math.abs(Math.pow((verticeAX - verticeCX),2)) + Math.abs(Math.pow((verticeAY - verticeCY),2));;
@@ -19,7 +72,15 @@ vincularEventos = () => {
     document.getElementById('aplicar-efeitos').addEventListener('click', (e) => {
         let tela = document.getElementById("tela");
         let cpx = tela.getContext("2d");
+
         var posA = oEsq.posA, tamA = oEsq.tamA, posB = oEsq.posB, tamB = oEsq.tamB, posC = oEsq.posC, tamC = oEsq.tamC;
+        var oLargura = rostoGato.largura, oAltura = rostoGato.altura, oTamanho = rostoGato.tamanho;
+        var eLargura = rostoGato.largura, eAltura = rostoGato.altura, eTamanho = rostoGato.tamanho;
+        var vLargura = dRetina.largura, vAltura = dRetina.altura, vTamanho = dRetina.tamanho, vLados = dRetina.lados;
+        var aposA = oEsq.posA, atamA = oEsq.tamA, aposB = oEsq.posB, atamB = oEsq.tamB, aposC = oEsq.posC, atamC = oEsq.tamC;
+
+        var orelhaEsqueda = this.orelhas(oEsq.posA, oEsq.tamA, oEsq.posB, oEsq.tamB, oEsq.posC, oEsq.tamC);
+        var rosto = this.rosto(rostoGato.largura, rostoGato.altura, rostoGato.tamanho);
         var tempo = 1;
         var sair = true;
     
@@ -35,12 +96,10 @@ vincularEventos = () => {
             let orelhaDireita = this.orelhas(posA, tamA, posB, tamB, posC, tamC);
             tempo++;
             setTimeout(function(){
-                let orelhaEsqueda = this.orelhas(oEsq.posA, oEsq.tamA, oEsq.posB, oEsq.tamB, oEsq.posC, oEsq.tamC);
-                
                 cpx.clearRect(0,0,tela.width,tela.height);
-                
-                cpx.stroke(orelhaDireita);
                 cpx.stroke(orelhaEsqueda);
+                cpx.stroke(orelhaDireita);
+                cpx.stroke(rosto);
             }, 10 * tempo);
 
             //sair do loop
@@ -49,48 +108,136 @@ vincularEventos = () => {
                 sair = false;
             }
         }
+
+        sair = true;
+
+        while(sair){
+            
+            if (oLargura !== oGatoDireito.largura) if (rostoGato.largura > oGatoDireito.largura){ oLargura--; }else if(rostoGato.largura < oGatoDireito.largura){ oLargura++ };
+            if (oAltura !== oGatoDireito.altura) if (rostoGato.largura > oGatoDireito.largura){ oAltura--; }else if(rostoGato.largura < oGatoDireito.largura){ oAltura++ };
+            if (oTamanho !== oGatoDireito.tamanho) if (rostoGato.tamanho > oGatoDireito.tamanho){ oTamanho--; }else if(rostoGato.tamanho < oGatoDireito.tamanho){ oTamanho++ };
+            
+            let bolinhas = this.rosto(oLargura, oAltura, oTamanho);
+            tempo++;
+            setTimeout(function(){
+                cpx.clearRect(0,0,tela.width,tela.height);
+                cpx.stroke(bolinhas);
+                cpx.stroke(orelhaEsqueda);
+                cpx.stroke(this.orelhas(posA, tamA, posB, tamB, posC, tamC));
+                cpx.stroke(rosto);
+            }, 10 * tempo);
+
+            //sair do loop
+            if ((oLargura ===  oGatoDireito.largura) && (oAltura ===  oGatoDireito.altura) &&  (oTamanho ===  oGatoDireito.tamanho)){
+                sair = false;
+            }
+        }
+
+        sair = true;
+
+        while(sair){
+            
+            if (eLargura !== oGatoEsquerdo.largura) if (rostoGato.largura > oGatoEsquerdo.largura){ eLargura--; }else if(rostoGato.largura < oGatoEsquerdo.largura){ eLargura++ };
+            if (eAltura !== oGatoEsquerdo.altura) if (rostoGato.altura > oGatoEsquerdo.altura){ eAltura--; }else if(rostoGato.altura < oGatoEsquerdo.altura){ eAltura++ };
+            if (eTamanho !== oGatoEsquerdo.tamanho) if (rostoGato.tamanho > oGatoEsquerdo.tamanho){ eTamanho--; }else if(rostoGato.tamanho < oGatoEsquerdo.tamanho){ eTamanho++ };
+            
+            let ebolinhas = this.rosto(eLargura, eAltura, eTamanho);
+            tempo++;
+            setTimeout(function(){
+                cpx.clearRect(0,0,tela.width,tela.height);
+                cpx.stroke(this.rosto(oLargura, oAltura, oTamanho));
+                cpx.strokeRect(dRetina.lados, dRetina.altura, dRetina.largura, dRetina.tamanho);
+                cpx.stroke(ebolinhas);
+                cpx.stroke(orelhaEsqueda);
+                cpx.stroke(this.orelhas(posA, tamA, posB, tamB, posC, tamC));
+                cpx.stroke(rosto);
+            }, 10 * tempo);
+
+            //sair do loop
+            if ((eLargura ===  oGatoEsquerdo.largura) && (eAltura ===  oGatoEsquerdo.altura) &&  (eTamanho ===  oGatoEsquerdo.tamanho)){
+                sair = false;
+            }
+        }
+
+        sair = true;
+        var tempo2 = tempo;
+
+        while(sair){
+            
+            if (vLargura !== eRetina.largura) if (dRetina.largura > eRetina.largura){ vLargura--; }else if(dRetina.largura < eRetina.largura){ vLargura++ };
+            if (vAltura !== eRetina.altura) if (dRetina.altura > eRetina.altura){ vAltura--; }else if(dRetina.altura < eRetina.altura){ vAltura++ };
+            if (vTamanho !== eRetina.tamanho) if (dRetina.tamanho > eRetina.tamanho){ vTamanho--; }else if(dRetina.tamanho < eRetina.tamanho){ vTamanho++ };
+            if (vLados !== eRetina.lados) if (dRetina.lados > eRetina.lados){ vLados--; }else if(dRetina.lados < eRetina.lados){ vLados++ };
+
+            cpx.strokeRect(dRetina.lados, dRetina.altura, dRetina.largura, dRetina.tamanho);
+            tempo++;
+            setTimeout(function(){
+                cpx.clearRect(0,0,tela.width,tela.height);
+                cpx.strokeRect(vLados, vAltura, vLargura, vTamanho);
+                cpx.strokeRect(dRetina.lados, dRetina.altura, dRetina.largura, dRetina.tamanho);
+                cpx.stroke(this.rosto(oLargura, oAltura, oTamanho));
+                cpx.stroke(this.rosto(eLargura, eAltura, eTamanho));
+                cpx.stroke(orelhaEsqueda);
+                cpx.stroke(this.orelhas(posA, tamA, posB, tamB, posC, tamC));
+                cpx.stroke(rosto);
+                this.desenharNariz();
+            }, 10 * tempo);
+
+            //sair do loop
+            if ((vLargura ===  eRetina.largura) && (vAltura ===  eRetina.altura) 
+            &&  (vTamanho ===  eRetina.tamanho) && (vLados ===  eRetina.lados)){
+                sair = false;
+            }
+        }
+
+        sair = true;
+
+        while(sair){
+            
+            if (aposA !== nariz.posA) if (oEsq.posA > nariz.posA){ aposA--; }else if(oEsq.posA < nariz.posA){ aposA++ };
+            if (atamA !== nariz.tamA) if (oEsq.tamA > nariz.tamA){ atamA--; }else if(oEsq.tamA < nariz.tamA){ atamA++ };
+            if (aposB !== nariz.posB) if (oEsq.posB > nariz.posB){ aposB--; }else if(oEsq.posB < nariz.posB){ aposB++ };
+            if (atamB !== nariz.tamB) if (oEsq.tamB > nariz.tamB){ atamB--; }else if(oEsq.tamB < nariz.tamB){ atamB++ };
+            if (aposC !== nariz.posC) if (oEsq.posC > nariz.posC){ aposC--; }else if(oEsq.posC < nariz.posC){ aposC++ };
+            if (atamC !== nariz.tamC) if (oEsq.tamC > nariz.tamC){ atamC--; }else if(oEsq.tamC < nariz.tamC){ atamC++ };
+
+            let iorelhaDireita = this.orelhas(aposA, atamA, aposB, atamB, aposC, atamC);
+
+            tempo2++;
+            setTimeout(function(){
+                cpx.clearRect(0,0,tela.width,tela.height);
+                cpx.strokeRect(vLados, vAltura, vLargura, vTamanho);
+                cpx.strokeRect(dRetina.lados, dRetina.altura, dRetina.largura, dRetina.tamanho);
+                cpx.stroke(this.rosto(oLargura, oAltura, oTamanho));
+                cpx.stroke(this.rosto(eLargura, eAltura, eTamanho));
+                cpx.stroke(iorelhaDireita);
+                cpx.stroke(orelhaEsqueda);
+                cpx.stroke(this.orelhas(posA, tamA, posB, tamB, posC, tamC));
+                cpx.stroke(rosto);
+                this.desenharNariz();
+            }, 10 * tempo2);
+
+            //sair do loop
+            if ((aposA ===  nariz.posA) && (atamA ===  nariz.tamA) &&  (aposB ===  nariz.posB) &&
+                (atamB ===  nariz.tamB) && (aposC ===  nariz.posC) && (atamC ===  nariz.tamC)){
+                sair = false;
+            }
+        }  
     });
 }
 
-//posA: oEsq.posA + 380,
-//tamA: oEsq.tamA + 0,
-//posB: oEsq.posB + 240,
-//tamB: oEsq.tamB - 100,
-//posC: oEsq.posC + 230,
-//tamC: oEsq.tamC + 100,
-
-let oEsq = {
-        posA: 110,
-        tamA: 30,
-        posB: 130,
-        tamB: 220,
-        posC: 240,
-        tamC: 120 
-    },
-    oDir = {
-        posA: 490,
-        tamA: 30,
-        posB: 470,
-        tamB: 220,
-        posC: 370,
-        tamC: 120
-    };
 
 function carregada() {
     vincularEventos();
-
-    let tela = document.getElementById("tela");
-    let cpx = tela.getContext("2d");
-
-    let orelhaEsqueda = this.orelhas(oEsq.posA, oEsq.tamA, oEsq.posB, oEsq.tamB, oEsq.posC, oEsq.tamC);
-    let orelhaDireita = this.orelhas(oDir.posA, oDir.tamA, oDir.posB, oDir.tamB, oDir.posC, oDir.tamC);
-    
-    desenharRosto();
-
-    cpx.stroke(orelhaEsqueda);
-    cpx.stroke(orelhaDireita);
 };
 
+function rosto(lados, cimabaixo, largura) {
+    let cpx = new Path2D();
+    
+    cpx.arc(lados, cimabaixo, largura, 0, Math.PI * 2, true); // Lado, Cima Baixo, Largura
+    
+    return cpx;
+}
 
 function orelhas(posA, tamA, posB, tamB, posC, tamC) {
     let cpx = new Path2D();
@@ -103,68 +250,10 @@ function orelhas(posA, tamA, posB, tamB, posC, tamC) {
     return cpx;
 }
 
-function rotacionarOrelhas(posA, tamA, posB, tamB, posC, tamC) {
-    let cpx = new Path2D();
-
-    var rotacao = 10;
-
-    let verticeAX = posA;
-    let verticeAY = tamA;
-    let verticeBX = posB;
-    let verticeBY = tamB;
-    let verticeCX = posC;
-    let verticeCY = tamC;
-    //120 y
-    //160 x posb
-    verticeAX = (160 * (Math.cos(rotacao))) + (120 * (Math.sin(rotacao)));
-    verticeAY = (160 * (Math.sin(rotacao))) + (120 * (Math.cos(rotacao)));
-
-    verticeBX = (160 * (Math.cos(rotacao))) + (120 * (Math.sin(rotacao)));
-    verticeBY = (160 * (Math.sin(rotacao))) + (120 * (Math.cos(rotacao)));
-
-    verticeCX = (160 * (Math.cos(rotacao))) + (120 * (Math.sin(rotacao)));
-    verticeCY = (160 * (Math.sin(rotacao))) + (120 * (Math.cos(rotacao)));
-    
-    
-    cpx.moveTo(verticeAX, verticeAY);
-    cpx.lineTo(verticeBX, verticeBY);
-    cpx.lineTo(verticeCX, verticeCY);
-    cpx.lineTo(verticeAX, verticeAY);
-    
-    return cpx;
-}
-
-function desenharRosto() {
-    let tela = document.getElementById("tela");
-    let cpx = tela.getContext("2d");
-
-    cpx.beginPath();
-    cpx.arc(300, 240, 170, 0, Math.PI * 2, true); // Lado, Cima Baixo, Largura
-    
-    cpx.stroke();
-}
-
-function desenharOlhos() {
-    let tela = document.getElementById("tela");
-    let cpx = tela.getContext("2d");
-    
-    var teste = new Path2D();
-
-    teste.arc(220, 230, 30, 0, Math.PI * 2, true); // Lado, Cima Baixo, Largura
-    cpx.stroke(teste);
-}
-
-function desenharRetinas() {
-    let tela = document.getElementById("tela");
-    let cpx = tela.getContext("2d");
-
-    cpx.strokeRect(217, 217, 05, 25); // (Lados (dir + esquerda - ) Cimabaixo (baixo + cima - ) Largura, Tamanho)
-}
-
 function desenharNariz() {
     let tela = document.getElementById("tela");
     let cpx = tela.getContext("2d");
 
-    cpx.strokeRect(270, 305, 50, 15); // (Lados (dir + esquerda - ) Cimabaixo (baixo + cima - ) Largura, Tamanho)
-    cpx.strokeRect(290, 320, 10, 22); // (Lados (dir + esquerda - ) Cimabaixo (baixo + cima - ) Largura, Tamanho)
+    cpx.strokeRect(270, 320, 60, 05); // (Lados (dir + esquerda - ) Cimabaixo (baixo + cima - ) Largura, Tamanho)
+    cpx.strokeRect(297, 300, 05, 20); // (Lados (dir + esquerda - ) Cimabaixo (baixo + cima - ) Largura, Tamanho)
 }
